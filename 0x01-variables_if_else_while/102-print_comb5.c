ba223487f8bc5499ed3a7de5f45bfeef8702a632
Paulharyor23 <sukuruaghogho@gmail.com>
2022-09-13 21:55:31 -0700

    Fix

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,7 +5,7 @@
  */
 int main(void)
 {
-	int i, j;
+int i, j;
 	
 	for (i = 0; i < 100; i++)
 	{
@@ -17,7 +17,7 @@ int main(void)
 				putchar((i % 10) + 48);
 				putchar(' ');
 				putchar((j / 10) + 48);
-				putchar((j % 10) + 48);
+				putchar ((j % 10) + 48);
 				if(i != 98 || j != 99)
 				{
 					putchar(',');
