8e7d71efe6f09d7c61ca47f4323a9042cf082e15
Paulharyor23 <sukuruaghogho@gmail.com>
2022-09-13 20:54:05 -0700

    Done

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,15 +13,15 @@ int main(void)
 	{
 		for (m = 49; m <= 57; m++)
 		{
-			if (m > n)														
+			if (m > n)
 			{														
 				putchar(n);		
 				putchar(m);
-				if (n != 56 || m != 57)												
-				{														
+				if (n != 56 || m != 57)
+				{
 					putchar(',');
 					putchar(' ');
-				}														
+				}
 			}
 		}
 	}
