b568b7c2ae13aaea4e5e287dcea7794103b1810f
Paulharyor23 <sukuruaghogho@gmail.com>
2022-09-16 03:25:44 -0700

    Delivered

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,32 +1,23 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- * print_triangle -  a function that prints a triangle, followed by a new line.
- * @size: An input integer
- * Return: Always 0
- *
+ * main - prints largest prime factor.
+ * Return: Always 0.
  */
-void print_triangle(int size)
-{
-	int i = 0, j, n = size - 1;
 
-	if (size > 0)
+int main(void)
+{
+	long int n, fp;
+
+	n = 612852475143;
+	for (fp = 2; fp <= n; fp++)
 	{
-		for (; i < size; i++)
+		if (n % fp == 0)
 		{
-			for (j = 0; j < size; j++)
-			{
-				if (j < n)
-					_putchar(' ');
-				else
-					_putchar('#');
-				{
-					n--;
-					_putchar('\n');
-				}
-			}
-			else
-				_putchar('\n');
+			n /= fp;
+			fp--;
 		}
 	}
+	printf("%ld\n", fp);
+	return (0);
 }
