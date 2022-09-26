9915e808d896fd7ac05a1953fa79d6cf47bbf24c
Paulharyor23 <sukuruaghogho@gmail.com>
2022-09-26 13:51:54 -0700

    Task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,46 +1,32 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * compare - function that compares two strings.
- * @haystack: pointer to the first null-terminated byte string.
- * @needle: pointer to the second null-terminated byte string.
+ * _strstr - locates a substring
+ * @haystack: string in which to check for needle
+ * @needle: substring to find in haystack
  *
- * Return: true if haystack and needle are the same.
- */
-int compare(char *haystack, char *needle)
-{
-	/* loop through both strings */
-	while (*haystack && *needle)
-	{
-		/* compare both strings */
-		if (*haystack != *needle)
-			return (0);
-
-		haystack++;
-		needle++;
-	}
-	return (*needle == '\0');
-}
-
-
-/**
- *_strstr - function that locates a substring.
- * @haystack: pointer to the null-terminated byte string to examine.
- * @needle: pointer to the null-terminated byte string to search for.
- *
- * Return:  a pointer to the beginning of the located substring,
- * or NULL if the substring is not found.
+ * Return: pointer to beginning of needle in haystack or NULL if no match
  */
 char *_strstr(char *haystack, char *needle)
 {
-	while (*haystack != '\0')
+	unsigned int i = 0, j = 0;
+
+	while (haystack[i])
 	{
-		if ((*haystack == *needle) && compare(haystack, needle))
+		while (needle[j] && (haystack[i] == needle[0]))
 		{
-			return (haystack);
+			if (haystack[i + j] == needle[j])
+				j++;
+			else
+				break;
 		}
-		haystack++;
+		if (needle[j])
+		{
+			i++;
+			j = 0;
+		}
+		else
+			return (haystack + i);
 	}
-	return ('\0');
+	return (0);
 }
