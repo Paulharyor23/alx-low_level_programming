611d98ba49d059a9746253edbf0f6c982922459b
Paulharyor23 <sukuruaghogho@gmail.com>
2022-09-30 14:01:38 -0700

    Task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -28,78 +28,51 @@ int _atoi(char *s)
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			digit = s[i] - '0';
-
-																											if (d % 2)
-
-																																digit = -digit;
-
-																														n = n * 10 + digit;
-
-																																	f = 1;
-
-																																				if (s[i + 1] < '0' || s[i + 1] > '9')
-
-																																									break;
-
-																																							f = 0;
-
-																																									}
-
-																			i++;
-
-																				}
-
-
-
-											if (f == 0)
-
-														return (0);
-
-
-
-												return (n);
-
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
+	}
+	if (f == 0)
+		return (0);
+	return (n);
 }
 
-
-
 /**
+ * main - adds two positive number
+ * @argc: number of arguments
+ * @argv: array of arguents
  *
- *  * main - adds two positive number
- *
- *   * @argc: number of arguments
- *
- *    * @argv: array of arguents
- *
- *     *
- *
- *      * Return: 0 (Success), or 1 (Success)
- *
- *       */
+ * Return: 0 (Success), or 1 (Success)
+ */
 int main(int argc, char *argv[])
 {
-		int sum, num, i, j, k;
-			sum = 0;
-				for (i = 1; i < argc; i++)
-						{
-									for (j = 0; argv[i][j] != '\0'; j++)
-												{
-																if (argv[i][j] > '9' || argv[i][j] < '0')
-																{
-																									puts("Error");
-																													return (1);
-																																}
-																		}										}
+	int sum, num, i, j, k;
 
-					for (k = 1; k < argc; k++)
-							{
-										num = _atoi(argv[k]);
-												if (num >= 0)
-															{
-																			sum += num;
-		
-													}
-
-						printf("%d\n", sum);
-							return (0);
+	sum = 0;
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] > '9' || argv[i][j] < '0')
+			{
+				puts("Error");
+				return (1);
+			}
+		}
+	}
+	for (k = 1; k < argc; k++)
+	{
+		num = _atoi(argv[k]);
+		if (num >= 0)
+		{
+			sum += num;
+		}
+		printf("%d\n", sum);
+		return (0);
 }
